3b851e36573c6647fb81c62c8db77a33c78249b9
unknown <Cameron Garnham@.(none)>
2011-07-09 16:26:39 +1000

    Re-Worked From Scratch

---
+++ bitcoin.com.au/bitcoin.com.au/Controllers/NavController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bitcoin.com.au.Controllers
+{
+    public class NavController : Controller
+    {
+        //
+        // GET: /Nav/
+
+        public ActionResult Menu()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ bitcoin.com.au/bitcoin.com.au/obj/Debug/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="bitcoin.com.au.MvcApplication" Language="C#" %>
